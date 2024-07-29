26f17202c6223226aef6006c5664943a25a9efc8
hlop3z <23062270+hlop3z@users.noreply.github.com>
2024-07-29 02:17:36 -0500

    core

--- index.ts
+++ index.ts
@@ -1,3 +1,94 @@
+// @ts-nocheck
 import Plugin from "./src/__init__.ts";
 
-window.TEST = Plugin;
+console.log(Plugin);
+
+const demoComponent = {
+  base: {
+    class: "button",
+    style: "",
+  },
+  attrs: (props) => ({
+    type: props.type ?? "button",
+  }),
+  setup: ({ size, disabled, height }) => ({
+    class: {
+      "is-small": size === "sm",
+      "is-large": size === "lg",
+      "is-disabled": disabled,
+    },
+    style: { height: height },
+  }),
+  theme: {
+    active: {
+      class: "color-bg-success",
+      style: "",
+    },
+    error: {
+      class: "color-bg-danger",
+      style: "width: 300px",
+    },
+  },
+};
+
+const component = Plugin.component(demoComponent);
+
+const Button = component({
+  type: "submit",
+  size: "sm",
+  disabled: true,
+  height: "40px",
+});
+
+console.log(Button);
+
+setTimeout(() => {
+  const active = Button.theme("active");
+  const error = Button.theme("error");
+  console.log(active);
+  console.log(error);
+}, 1000);
+
+Plugin.get("layout", "left")
+  ?.css({
+    active: {
+      class: "color-bg-success",
+      style: "height: 300px",
+    },
+    error: {
+      class: "color-bg-danger",
+      style: "",
+    },
+  })
+  .theme("error");
+
+setTimeout(() => {
+  const theme = Plugin.get("layout", "left")?.theme("active");
+});
+
+/*
+setInterval(() => {
+  Plugin.get("layout", "left")?.toggle("open");
+
+  Plugin.get("layout", "left").current.setAttribute(
+    "style",
+    "padding-top: 40px; padding-right: 20px"
+  );
+
+  console.log(Plugin.get("layout", "left").current);
+}, 1000);
+*/
+
+/*
+console.log(Plugin.class({ "is-small": true }));
+console.log(Plugin.class(["other", ["extra"], [{ "is-small": true }]]));
+console.log(
+  Plugin.style([
+    "width: 40;;;;;;;",
+    ["overflow-x: auto;", "overflow-y: auto;;"],
+    { height: "40px" },
+  ])
+);
+
+console.log(Plugin.blankForm({ key: "one", val: () => "hello world" }));
+*/
--- src/__init__.ts
+++ src/__init__.ts
@@ -1,4 +1,21 @@
 // @ts-nocheck
-import "./styles/__init__.scss";
+// import "./styles/__init__.scss";
 
-export default {};
+import { createAdmin, querySelector, querySelectorAll } from "./element";
+import { objectToClass, objectToStyle, Styled, blankForm } from "./styling";
+import theme from "./theme";
+
+export default {
+  // Elements
+  admin: createAdmin,
+  find: querySelectorAll,
+  get: querySelector,
+  // CSS
+  style: objectToStyle,
+  class: objectToClass,
+  // Complex
+  component: Styled,
+  blankForm: blankForm,
+  // Colors
+  theme: theme,
+};
